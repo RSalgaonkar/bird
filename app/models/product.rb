4ac464780c3b218d0bdf48b6d1404d3a186988d7
Rashmith <rashmith@rashmith-Inspiron-N5050.(none)>
2014-08-11 23:43:50 +0530

    check if the product code is master product code or sub product code

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ActiveRecord::Base
 
   belongs_to :master_product, class_name: "Product"
   has_many :sub_products, class_name: "Product", foreign_key: "master_product_id"
+
+  def is_master?
+  	master_product_id == nil or master_product_id == ''
+  end
+
+  def is_sub_product?
+  	master_product_id != nil
+  end
 end
